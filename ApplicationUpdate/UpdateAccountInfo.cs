e56142a4733020803a97f571345a8332a4edc69a
angelfv <angelfv99@gmail.com>
2014-12-30 16:52:01 -0400

    Libraries

--- ApplicationUpdate/UpdateAccountInfo.cs
+++ ApplicationUpdate/UpdateAccountInfo.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Activities;
+
+
+
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk.Messages;
+
+
+using Microsoft.Crm.Sdk.Proxy;
 
 namespace ApplicationUpdate
 {
